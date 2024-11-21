1662c40734405379ca6b9136cda90f1570ca7682
Nursultan <nzhalgasuly@gmail.com>
2024-11-22 00:09:17 +0500

    a

--- JavaRestAPI-master/postgre/src/main/java/com/nursultan/postgre/mappers/Mapper.java
+++
@@ -1,7 +0,0 @@
-package com.nursultan.postgre.mappers;
-
-public interface Mapper<A,B> {
-    B mapTo(A a);
-
-    A mapFrom(B b);
-}
--- JavaRestAPI-master/postgre/src/main/java/com/nursultan/postgre/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nursultan.postgre.repository;
-
-import com.nursultan.postgre.domain.entities.AuthorEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends CrudRepository<AuthorEntity, Long> {
-}
